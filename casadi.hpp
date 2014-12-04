4eff4fc9f64b9ccdead9ec90dd5194af50736481
Abuenameh Aiyejina <abuenameh@gmail.com>
2014-12-04 02:11:56 -0400

    Commit

--- casadi.hpp
+++ casadi.hpp
@@ -25,35 +25,87 @@ using namespace boost::posix_time;
 
 #include "gutzwiller.hpp"
 
+class GroundStateProblem {
+public:
+    GroundStateProblem();
+
+    //    void setParameters(double U0, vector<double>& dU, vector<double>& J, double mu);
+    void setParameters(double W, vector<double>& xi, double mu);
+    void setTheta(double theta);
+
+    double E(const vector<double>& f, vector<double>& grad);
+
+    string& getStatus() {
+        return status;
+    }
+    string getRuntime();
+
+    void start() {
+        start_time = microsec_clock::local_time();
+    }
+
+    void stop() {
+        stop_time = microsec_clock::local_time();
+    }
+
+private:
+
+    ptime start_time;
+    ptime stop_time;
+
+    SX energy();
+
+    vector<SX> fin;
+    SX W;
+    SX U0;
+    vector<SX> dU;
+    vector<SX> J;
+    vector<SX> xi;
+    SX mu;
+    SX theta;
+
+    SX x;
+    SX p;
+
+    vector<double> params;
+
+    SXFunction Ef;
+    Function Egradf;
+
+    string status;
+    double runtime;
+};
+
+
 class DynamicsProblem {
 public:
     DynamicsProblem();
-//    ~DynamicsProblem() { delete integrator; }
-    
-//    void setParameters(double U0, vector<double>& dU, vector<double>& J, double mu);
-void setParameters(double Wi, double Wf, double tau, vector<double>& xi, double mu);
-void setInitial(vector<double>& f0);
+        ~DynamicsProblem() { delete integrator; delete gsprob; }
 
-void evolve();
-    
-//    double solve(vector<double>& f);
-//    
-//    double E(const vector<double>& f, vector<double>& grad);
-    
-    string& getStatus() { return status; }
+    //    void setParameters(double U0, vector<double>& dU, vector<double>& J, double mu);
+    void setParameters(double Wi, double Wf, double tau, vector<double>& xi, double mu);
+    void setInitial(vector<double>& f0);
+
+    void evolve();
+
     string getRuntime();
-    
-    void start() { start_time = microsec_clock::local_time(); }
-    void stop() { stop_time = microsec_clock::local_time(); }
-    
+
+    void start() {
+        start_time = microsec_clock::local_time();
+    }
+
+    void stop() {
+        stop_time = microsec_clock::local_time();
+    }
+
 private:
-    
+
     ptime start_time;
     ptime stop_time;
-    
+
     complex<SX> HS();
     SX W();
-    
+
     vector<SX> fin;
     SX U0;
     vector<SX> dU;
@@ -64,80 +116,32 @@ private:
     SX Wi;
     SX Wf;
     SX tau;
-    
+
     SX Wt;
-    
+
     SX t;
     SX x;
     SX p;
-    
+
     double tf;
-    
+
     SXFunction Ufunc;
     vector<SXFunction> Jfunc;
-    
+
     double U0d;
     vector<double> Jd;
-    
+
     SX ode;
     SXFunction ode_func;
     CvodesInterface* integrator;
-    
+
     vector<double> params;
     vector<double> x0;
     
-    SXFunction Ef;
-    Function Egradf;
-    NlpSolver nlp;
-    
-    string status;
-    double runtime;
-};
+    GroundStateProblem* gsprob;
 
-class GroundStateProblem {
-public:
-    GroundStateProblem();
-    
-//    void setParameters(double U0, vector<double>& dU, vector<double>& J, double mu);
-    void setParameters(double W, vector<double>& xi, double mu);
-    void setTheta(double theta);
-    
-    double solve(vector<double>& f);
-    
-    double E(const vector<double>& f, vector<double>& grad);
-    
-    string& getStatus() { return status; }
-    string getRuntime();
-    
-    void start() { start_time = microsec_clock::local_time(); }
-    void stop() { stop_time = microsec_clock::local_time(); }
-    
-private:
-    
-    ptime start_time;
-    ptime stop_time;
-    
-    SX energy();
-    
-    vector<SX> fin;
-    SX W;
-    SX U0;
-    vector<SX> dU;
-    vector<SX> J;
-    vector<SX> xi;
-    SX mu;
-    SX theta;
-    
-    SX x;
-    SX p;
-    
-    vector<double> params;
-    
     SXFunction Ef;
-    Function Egradf;
-//    NlpSolver nlp;
-    
-    string status;
+
     double runtime;
 };
 
